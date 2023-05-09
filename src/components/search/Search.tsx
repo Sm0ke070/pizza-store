0bf1610b768e4f968d7b5a4a47d9551f2e402c0b
Sm0ke070 <smokedeveloper070@gmail.com>
2023-05-09 05:42:40 +0300

    added debounce in search

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,30 +1,42 @@
-import React, {FC, useContext, useRef} from 'react';
-import styles from './search.module.scss'
-import searchIcon from '../../assets/img/serach-input.svg'
-import closeIcon from '../../assets/img/clear-input.svg'
+import React, {ChangeEvent, FC, useCallback, useContext, useRef, useState} from 'react';
+import styles from './search.module.scss';
+import searchIcon from '../../assets/img/serach-input.svg';
+import closeIcon from '../../assets/img/clear-input.svg';
 import {ContextType, SearchContext} from "../../App";
-
+import debounce from 'lodash.debounce';
 
 const Search: FC = () => {
 
     const inputRef = useRef<HTMLInputElement>(null);
     const {searchValue, setSearchValue} = useContext<ContextType>(SearchContext)
+    const [value, setValue] = useState('')
 
     const onClickClear = () => {
+        setValue('')
         setSearchValue('')
         if (inputRef.current) {
             inputRef.current.focus()
         }
     }
+    const updateSearchValue = useCallback(
+        debounce((str: string) => {
+            setSearchValue(str)
+        }, 700), []
+    )
+    const onChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
+        setValue(event.currentTarget.value)
+        updateSearchValue(event.currentTarget.value)
+    }
 
     return (
         <div className={styles.root}>
             <img className={styles.searchIcon} src={searchIcon} alt="search-icon"/>
-            <input ref={inputRef} value={searchValue}
-                   onChange={(event) => setSearchValue(event.currentTarget.value)}
+            <input ref={inputRef}
+                   value={value}
+                   onChange={onChangeInput}
                    className={styles.input} type="text"
                    placeholder={'Поиск'}/>
-            {searchValue &&
+            {value && searchValue &&
                 <img onClick={onClickClear} className={styles.clearIcon} src={closeIcon}
                      alt="clear-input"/>
             }
